c45f08eef98d990b08b17aa3ec0107975c251bed
Muhammad Ali Haider <ali.haider@venturedive.com>
2024-09-02 08:27:49 +0500

    next_build  changes

--- next.config.js
+++ next.config.js
@@ -1,23 +1,48 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
 // module.exports = {
 //   assetPrefix: isProd ? '/TrioTechMinds.github.io/' : '',
 //   images: {
-//     unoptimized: true,
+//     unoptimized: true, // Disable Next.js' image optimization
 //   },
 //   basePath: isProd ? '/TrioTechMinds.github.io' : '',
 //   trailingSlash: true,
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.(png|jpg|jpeg|gif|svg)$/i,
+//       use: [
+//         {
+//           loader: 'file-loader',
+//           options: {
+//             name: '[path][name].[ext]',
+//             publicPath: '/_next/static/images/',
+//             outputPath: 'static/images/',
+//             esModule: false,
+//           },
+//         },
+//       ],
+//     });
+
+//     return config;
+//   },
 // };
 
+
+
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   assetPrefix: isProd ? '/TrioTechMinds.github.io/' : '',
   images: {
-    unoptimized: true,
+    unoptimized: true, // Disable Next.js' image optimization
   },
   basePath: isProd ? '/TrioTechMinds.github.io' : '',
   trailingSlash: true,
+  future: {
+    webpack5: true, // Enable Webpack 5
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.(png|jpg|jpeg|gif|svg)$/i,
@@ -37,7 +62,3 @@ module.exports = {
     return config;
   },
 };
-
-
-
-
